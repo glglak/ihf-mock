51a2178f80207dc6d7f1deaedbbabf75072547ab
glglak <karem.kamel@gmail.com>
2025-04-28 11:55:13 +0300

    Add src/App.js

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          {/* Add more routes as needed */}
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
