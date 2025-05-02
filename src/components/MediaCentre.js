4c7363b40462ff6969f63ec540ef61ff74edfb0d
glglak <karem.kamel@gmail.com>
2025-05-02 03:40:48 +0300

    Fix unused imports in MediaCentre component

--- src/components/MediaCentre.js
+++ src/components/MediaCentre.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import { FaArrowRight, FaEye } from 'react-icons/fa';
 import './MediaCentre.css';
 import ApiService from '../services/api.service';
