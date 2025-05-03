390e3f7b83f3e8886f50a95c5929e1670eee1cb6
glglak <karem.kamel@gmail.com>
2025-05-03 16:43:27 +0300

    Update Router with basename for GitHub Pages deployment

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="app">
         <Header />
         <main className="main-content">
