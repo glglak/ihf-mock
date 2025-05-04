2aa42fa127178b2471d2301f72e552956fa4a6d5
glglak <karem.kamel@gmail.com>
2025-05-04 04:06:30 +0300

    Update Header component to match design in Image 1

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -7,18 +7,17 @@ import './Header.css';
 const Header = () => {
   return (
     <header className="site-header">
-      <Navbar expand="lg" className="navbar-light bg-white">
-        <Container>
-          <Navbar.Brand as={Link} to="/">
-            <div className="logo-container">
+      <div className="top-header">
+        <Container className="d-flex justify-content-between align-items-center">
+          <div className="logo-container">
+            <Link to="/">
               <img 
-                src="https://www.ihf.info/themes/ihf_theme/assets/img/logo.png?739"
-                alt="IHF Logo"
+                src="/ihf-logo-full.png"
+                alt="International Handball Federation"
                 className="header-logo"
-                crossOrigin="anonymous"
               />
-            </div>
-          </Navbar.Brand>
+            </Link>
+          </div>
           
           <div className="social-header-links">
             <a href="https://facebook.com/ihf" className="social-icon"><FaFacebook /></a>
@@ -26,49 +25,60 @@ const Header = () => {
             <a href="https://youtube.com/ihf" className="social-icon"><FaYoutube /></a>
             <a href="https://instagram.com/ihf" className="social-icon"><FaInstagram /></a>
             <a href="https://tiktok.com/@ihf" className="social-icon"><FaTiktok /></a>
-            <div className="header-right-icons">
-              <a href="/login" className="login-icon"><FaUser /></a>
-              <a href="/search" className="search-icon"><FaSearch /></a>
+            <div className="login-container">
+              <Link to="/login" className="login-link">
+                <FaUser className="login-icon" />
+                <span>Login</span>
+              </Link>
             </div>
           </div>
+        </Container>
+      </div>
+      
+      <Navbar expand="lg" className="main-navbar" variant="dark">
+        <Container>
+          <Navbar.Toggle aria-controls="main-navbar-nav" />
           
-          <Navbar.Toggle aria-controls="main-navbar" />
-          
-          <Navbar.Collapse id="main-navbar" className="main-navigation">
-            <Nav className="mx-auto">
-              <Nav.Link as={Link} to="/" className="nav-item">HOME</Nav.Link>
+          <Navbar.Collapse id="main-navbar-nav">
+            <Nav className="me-auto navigation-links">
+              <Nav.Link as={Link} to="/">HOME</Nav.Link>
               
-              <NavDropdown title="ABOUT" id="about-dropdown" className="nav-item">
+              <NavDropdown title="ABOUT" id="about-dropdown">
                 <NavDropdown.Item as={Link} to="/about/history">History</NavDropdown.Item>
                 <NavDropdown.Item as={Link} to="/about/mission">Mission</NavDropdown.Item>
               </NavDropdown>
               
-              <Nav.Link as={Link} to="/federations" className="nav-item">FEDERATIONS</Nav.Link>
-              <Nav.Link as={Link} to="/competitions" className="nav-item">COMPETITIONS</Nav.Link>
+              <Nav.Link as={Link} to="/federations">FEDERATIONS</Nav.Link>
+              <Nav.Link as={Link} to="/competitions">COMPETITIONS</Nav.Link>
               
-              <NavDropdown title="ANTI DOPING" id="anti-doping-dropdown" className="nav-item">
+              <NavDropdown title="ANTI DOPING" id="anti-doping-dropdown">
                 <NavDropdown.Item as={Link} to="/anti-doping/policy">Policy</NavDropdown.Item>
                 <NavDropdown.Item as={Link} to="/anti-doping/education">Education</NavDropdown.Item>
               </NavDropdown>
               
-              <Nav.Link as={Link} to="/statutes" className="nav-item">STATUTES & REGULATIONS</Nav.Link>
-              <Nav.Link as={Link} to="/marketing" className="nav-item">MARKETING</Nav.Link>
+              <Nav.Link as={Link} to="/statutes">STATUTES & REGULATIONS</Nav.Link>
+              <Nav.Link as={Link} to="/marketing">MARKETING</Nav.Link>
               
-              <NavDropdown title="MEDIA CENTRE" id="media-centre-dropdown" className="nav-item">
+              <NavDropdown title="MEDIA CENTRE" id="media-centre-dropdown">
                 <NavDropdown.Item as={Link} to="/media/gallery">Gallery</NavDropdown.Item>
                 <NavDropdown.Item as={Link} to="/media/videos">Videos</NavDropdown.Item>
               </NavDropdown>
               
-              <NavDropdown title="AWARDS" id="awards-dropdown" className="nav-item">
+              <NavDropdown title="AWARDS" id="awards-dropdown">
                 <NavDropdown.Item as={Link} to="/awards/world-players">World Players</NavDropdown.Item>
                 <NavDropdown.Item as={Link} to="/awards/hall-of-fame">Hall of Fame</NavDropdown.Item>
               </NavDropdown>
               
-              <NavDropdown title="GAMES" id="games-dropdown" className="nav-item">
+              <NavDropdown title="GAMES" id="games-dropdown">
                 <NavDropdown.Item as={Link} to="/games/world-games">World Games</NavDropdown.Item>
                 <NavDropdown.Item as={Link} to="/games/olympic-games">Olympic Games</NavDropdown.Item>
               </NavDropdown>
             </Nav>
+            <div className="search-container">
+              <Link to="/search" className="search-link">
+                <FaSearch className="search-icon" />
+              </Link>
+            </div>
           </Navbar.Collapse>
         </Container>
       </Navbar>
