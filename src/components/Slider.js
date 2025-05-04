b768bf160ecca53b8aee44f4f583a492da8173bc
glglak <karem.kamel@gmail.com>
2025-05-04 04:20:47 +0300

    Updated Slider.js to properly handle image URLs

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import { Container, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './Slider.css';
 
